0954f82b2fb1e96887fda1533c79152f344cb7b8
Vedran Sustic <78386759+vedransustic@users.noreply.github.com>
2021-10-30 13:37:45 +0200

    Added cryptoStats and redux and API query aka custom react hook

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import millify from 'millify'
 import { Typography, Row, Col, Statistic } from "antd";
-import { Link } from 'react-router-dom'
+//import { Link } from 'react-router-dom'
 import { useGetCryptosQuery } from '../services'
 import { LoadingOutlined } from "@ant-design/icons";
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Button, Menu, Typography, Avatar} from 'antd';
+import { Menu, Typography, Avatar} from 'antd';
 import { Link } from 'react-router-dom'
-import { HomeOutlined, TransactionOutlined, BulbOutlined, WalletOutlined, StockOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+import { HomeOutlined, TransactionOutlined, BulbOutlined, WalletOutlined, StockOutlined } from "@ant-design/icons";
 import icon from '../img/cryptocurrency.png'
 
 const Navbar = () => {
@@ -15,19 +15,19 @@ const Navbar = () => {
 
             </div>
             <Menu theme="dark">
-                <Menu.Item icon={<HomeOutlined />}>
+                <Menu.Item icon={<HomeOutlined />} key={"home"}>
                     <Link to="/">Home</Link>
                 </Menu.Item>
-                <Menu.Item icon={<WalletOutlined />}>
+                <Menu.Item icon={<WalletOutlined />} key={"cryptocurrencies"}>
                     <Link to="/cryptocurrencies">Cryptocurrencies</Link>
                 </Menu.Item>
-                <Menu.Item icon={<TransactionOutlined />}>
+                <Menu.Item icon={<TransactionOutlined />} key={"exchanges"}>
                     <Link to="/exchanges">Exchanges</Link>
                 </Menu.Item>
-                <Menu.Item icon={<StockOutlined />}>
+                <Menu.Item icon={<StockOutlined />} key={"stocks"}>
                     <Link to="/stocks">Stocks</Link>
                 </Menu.Item>
-                <Menu.Item icon={<BulbOutlined />}>
+                <Menu.Item icon={<BulbOutlined />} key={"news"}>
                     <Link to="/news">News</Link>
                 </Menu.Item>
             </Menu>
